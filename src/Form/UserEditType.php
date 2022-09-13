48ed21edb3006c428f1cbec1b2030209e92de2a8
GBRL-Web <gabi.titi.dnd@gmail.com>
2022-09-13 17:50:58 +0300

    PHP Twig project for fake cleaning company.

---
+++ src/Form/UserEditType.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+
+class UserEditType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('role', ChoiceType::class, [
+                'choices' => [
+                    'Novice' => 'ROLE_USER_NOVICE',
+                    'Senior' => 'ROLE_USER',
+                    'Administrator' => 'ROLE_ADMIN'
+                ],
+
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
